960390b18e29529bd822c303db01ce91679b6523
Fabio Moro <fabio.moro2000@gmail.com>
2022-05-16 12:22:46 +0200

    prima versione

---
+++ snack-1/main.js
@@ -0,0 +1,58 @@
+const zucchine = [
+    {
+        peso : 5,
+        lunghezza : 10,
+    },
+    
+    {
+        peso : 5,
+        lunghezza : 10,
+    },
+    
+    {
+        peso : 5,
+        lunghezza : 10,
+    },
+    
+    {
+        peso : 5,
+        lunghezza : 10,
+    },
+    
+    {
+        peso : 5,
+        lunghezza : 10,
+    },
+    
+    {
+        peso : 5,
+        lunghezza : 10,
+    },
+    
+    {
+        peso : 5,
+        lunghezza : 10,
+    },
+    
+    {
+        peso : 5,
+        lunghezza : 10,
+    },
+    
+    {
+        peso : 5,
+        lunghezza : 10,
+    },
+    
+    {
+        peso : 5,
+        lunghezza : 10,
+    },
+]
+
+let somma=0;
+for(let i=0; i < zucchine.length; i++) {
+    somma += zucchine[i].peso;
+}
+
+alert(somma);
